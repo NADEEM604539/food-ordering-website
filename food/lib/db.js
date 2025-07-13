5d6fdabfec3330f18df0ed133bf7e3039faeeb98
NADEEM604539 <ndm604539@gmail.com>
2025-07-13 14:00:16 +0500

    Update db.js

--- food/lib/db.js
+++ food/lib/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: 'gateway01.us-west-2.prod.aws.tidbcloud.com',
-  port: 4000,
-  user: '3heehgnHZK8CDig.root',
-  password: 'UpVM7wD5oYDeZN7z',
+  host: '',
+  port: ,
+  user: '',
+  password: '',
   database: 'foods',
   waitForConnections: true,
   connectionLimit: 10,
